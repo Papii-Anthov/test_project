1553747ff054eed5c342632e3fd3faa6b02892dc
Papii-Anthov <122486814+Papii-Anthov@users.noreply.github.com>
2023-04-19 18:56:51 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String buttonName = 'Click';
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('App title'),
+        ),
+        body: Center(
+          child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttonName = 'clicked';
+                    });
+                  },
+                  child: Text(buttonName),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttonName = 'clicked';
+                    });
+                  },
+                  child: Text(buttonName),
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home, color: Colors.blue ),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings, color: Colors.yellow),
+            ),
+            BottomNavigationBarItem(
+              label: 'Favorite',
+              icon: Icon(Icons.favorite, color: Colors.red),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
